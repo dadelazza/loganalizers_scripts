be79212711d2b0e9222d16b6c448fe8e75fdc1c9
Davide Lazzari <davide.lazzari@vivaticket.com>
2025-03-02 23:25:40 +0100

    renamed in .sh

---
+++ goaccess-iis-tomcat-logs.sh
@@ -0,0 +1,21 @@
+# Script per generare report di log IIS e Tomcat access log
+
+export LANG=en_US.UTF-8
+
+#report completo
+grep -h ".*" *.log | goaccess -o report-00AllISAPICalls.html --html-report-title="IIS: all accesses to any ISAPI of 2025-02-26" --log-format='%d %t %^ %m %U %q %^ %^ %h %u %R %s %^ %^ %^ %^ %L' --date-format=%Y-%m-%d --time-format=%H:%M:%S --enable-panel=VISIT_TIMES --ignore-panel=VISITORS --ignore-panel=HOSTS --ignore-panel=REQUESTS --ignore-panel=REQUESTS_STATIC --ignore-panel=REFERRERS --ignore-panel=REFERRING_SITES --ignore-panel=OS --ignore-panel=BROWSERS --ignore-panel=STATUS_CODES --ignore-panel=NOT_FOUND --html-prefs='{"theme":"bright","perPage":50,"layout":"vertical","showTables":false}' --max-items=100 --no-html-last-updated
+
+#solo l'api SearchCalendarAvailability
+grep -h "SearchCalendarAvailability" *.log | goaccess -o report-01SearchCalendarAvailability.html --html-report-title="IIS: SearchCalendarAvailability of 2025-02-26" --log-format='%d %t %^ %m %U %q %^ %^ %h %u %R %s %^ %^ %^ %^ %L' --date-format=%Y-%m-%d --time-format=%H:%M:%S --enable-panel=VISIT_TIMES --ignore-panel=VISITORS --ignore-panel=HOSTS --ignore-panel=REQUESTS --ignore-panel=REQUESTS_STATIC --ignore-panel=REFERRERS --ignore-panel=REFERRING_SITES --ignore-panel=OS --ignore-panel=BROWSERS --ignore-panel=STATUS_CODES --ignore-panel=NOT_FOUND --html-prefs='{"theme":"bright","perPage":50,"layout":"vertical","showTables":false}' --max-items=100 --no-html-last-updated
+
+#solo le righe senza verbo con errore 500
+grep -h "/BosServices\.dll/soap/IWsAPIPerformance -.*- 500 0.*" *.log | goaccess -o report-02Performance500.html --html-report-title="IIS: IWsAPIPerformance with error 500 of 2025-02-26" --log-format='%d %t %^ %m %U %q %^ %^ %h %u %R %s %^ %^ %^ %^ %L' --date-format=%Y-%m-%d --time-format=%H:%M:%S --enable-panel=VISIT_TIMES --ignore-panel=VISITORS --ignore-panel=HOSTS --ignore-panel=REQUESTS --ignore-panel=REQUESTS_STATIC --ignore-panel=REFERRERS --ignore-panel=REFERRING_SITES --ignore-panel=OS --ignore-panel=BROWSERS --ignore-panel=STATUS_CODES --ignore-panel=NOT_FOUND --html-prefs='{"theme":"bright","perPage":50,"layout":"vertical","showTables":false}' --max-items=100 --no-html-last-updated
+
+#solo l'api FindPerformanceCapacityAndProduct
+grep -h "FindPerformanceCapacityAndProduct" *.log | goaccess -o report-03FindPerformanceCapacityAndProduct.html --html-report-title="IIS: FindPerformanceCapacityAndProduct of 2025-02-26"  --log-format='%d %t %^ %m %U %q %^ %^ %h %u %R %s %^ %^ %^ %^ %L' --date-format=%Y-%m-%d --time-format=%H:%M:%S --enable-panel=VISIT_TIMES --ignore-panel=VISITORS --ignore-panel=HOSTS --ignore-panel=REQUESTS --ignore-panel=REQUESTS_STATIC --ignore-panel=REFERRERS --ignore-panel=REFERRING_SITES --ignore-panel=OS --ignore-panel=BROWSERS --ignore-panel=STATUS_CODES --ignore-panel=NOT_FOUND --html-prefs='{"theme":"bright","perPage":50,"layout":"vertical","showTables":false}' --max-items=100 --no-html-last-updated
+
+#access log tomcat completo
+grep -h ".*" *.txt | goaccess -o report_tomcat-00_All.html --html-report-title="Tomcat: All accesses of resource of 2025-02-26" --log-format=COMMON --enable-panel=VISIT_TIMES --ignore-panel=VISITORS --ignore-panel=HOSTS --ignore-panel=REQUESTS --ignore-panel=REQUESTS_STATIC --ignore-panel=REFERRERS --ignore-panel=REFERRING_SITES --ignore-panel=OS --ignore-panel=BROWSERS --ignore-panel=STATUS_CODES --ignore-panel=NOT_FOUND --html-prefs='{"theme":"bright","perPage":50,"layout":"vertical","showTables":false}' --max-items=100 --no-html-last-updated
+
+#access log Tomcat: solo accessi a buy_tickets
+grep -h "buy-tickets\.html" *.txt | goaccess -o report_tomcat-01_buy-tickets.html --html-report-title="Tomcat: buy-tickets.html of 2025-02-26" --log-format=COMMON --enable-panel=VISIT_TIMES --ignore-panel=VISITORS --ignore-panel=HOSTS --ignore-panel=REQUESTS --ignore-panel=REQUESTS_STATIC --ignore-panel=REFERRERS --ignore-panel=REFERRING_SITES --ignore-panel=OS --ignore-panel=BROWSERS --ignore-panel=STATUS_CODES --ignore-panel=NOT_FOUND --html-prefs='{"theme":"bright","perPage":50,"layout":"vertical","showTables":false}' --max-items=100 --no-html-last-updated
